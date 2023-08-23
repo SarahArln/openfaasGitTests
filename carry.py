896540d0ad51dc1458839f88e1c921b550b128dd
Your Name <1@example.com>
2023-08-23 15:59:27 +0800

    🎉 init(init): init

---
+++ carry.py
@@ -0,0 +1,23 @@
+import requests
+import json
+def handle(req):
+    listener_ip = '10.0.0.123'
+    database_ip = '10.0.0.123'
+    openfaas_ip = 'http://10.0.0.123:31112/function/openfaas'
+    data = {
+        "ip": listener_ip,
+        "device": "magician",
+        "method": "suck on",
+        "user": "siemens",
+        "pwd": "siemens"
+    }
+    response1 = requests.get(url=openfaas_ip, data=json.dumps(data))
+    data = {
+        "ip": listener_ip,
+        "device": "magician",
+        "method": "suck off",
+        "user": "siemens",
+        "pwd": "siemens"
+    }
+    response2 = requests.get(url=openfaas_ip, data=json.dumps(data))
+    return response1.text + '\n' + response2.text + '123'
